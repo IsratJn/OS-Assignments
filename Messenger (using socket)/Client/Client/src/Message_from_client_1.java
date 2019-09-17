fb08e3702a7c3ccd7c641f6ea426b555f02a1792
Israt33 <42838747+IsratJn@users.noreply.github.com>
2019-09-17 09:28:07 +0600

    Add files via upload

---
+++ Messenger (using socket)/Client/Client/src/Message_from_client_1.java
@@ -0,0 +1,9 @@
+public class Message_from_client_1
+{
+
+    public static void main(String[] args) 
+    {
+        Message_from_client client=new Message_from_client("192.168.1.19");
+        client.startRunning();
+    }
+}
